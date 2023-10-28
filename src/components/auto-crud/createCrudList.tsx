bf528123bf5ba03846cd2925969c7c6e0b037870
LunaTK <ahn_v3@naver.com>
2023-10-28 15:52:21 +0900

    use icons for button

--- src/components/auto-crud/createCrudList.tsx
+++ src/components/auto-crud/createCrudList.tsx
@@ -1,15 +1,18 @@
 import { Crud, CrudListComponent } from './type'
 import { ColumnDef } from '@tanstack/react-table'
-import { DotsHorizontalIcon } from '@radix-ui/react-icons'
+import { Pencil1Icon, TrashIcon, PlusIcon } from '@radix-ui/react-icons'
 import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { DataTable } from '../ui/data-table'
 
 export const createCrudList = <T extends Crud>({ columns }: { columns: () => ColumnDef<T['listItem']>[] }) => {
   const CrudListTable: CrudListComponent<T> = ({ dataSource, update, del, create }) => {
     return (
       <DataTable
-        toolbar={<Button onClick={create}>New</Button>}
+        toolbar={
+          <Button onClick={create}>
+            <PlusIcon className="w-4 h-4 mr-2" /> New
+          </Button>
+        }
         columns={[
           ...columns(),
           {
@@ -19,20 +22,14 @@ export const createCrudList = <T extends Crud>({ columns }: { columns: () => Col
             header: () => 'Action',
             cell: ({ row }) => {
               return (
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="w-8 h-8 p-0">
-                      <span className="sr-only">Open menu</span>
-                      <DotsHorizontalIcon className="w-4 h-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuItem onClick={() => update(row)}>Edit</DropdownMenuItem>
-                    <DropdownMenuItem className="text-destructive" onClick={() => confirm('Sure to delete?') && del(row)}>
-                      Delete
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
+                <>
+                  <Button onClick={() => update(row)} variant="ghost" size="icon">
+                    <Pencil1Icon className="w-4 h-4" />
+                  </Button>
+                  <Button variant="ghost" onClick={() => confirm('Sure to delete?') && del(row)} size="icon">
+                    <TrashIcon className="w-4 h-4 text-destructive" />
+                  </Button>
+                </>
               )
             },
           },
